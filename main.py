82c829ee5b2e8fb78e38966e4184fb8aa76aa54f
Gustavo Morais <gustavombdn@gmail.com>
2026-09-15 22:00:41 -0300

    adds API key reading and start function

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+async def start(update, context):
+    await update.message.reply_text('Hello, World!')
